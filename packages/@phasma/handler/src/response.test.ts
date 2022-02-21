4552ba3b9f85b044a5941daa500464ce8d2480a5
Matthew Rose <matt.usurp@gmail.com>
2022-02-21 12:35:30 +0000

    feat(handler): unwrap response helper

--- packages/@phasma/handler/src/response.test.ts
+++ packages/@phasma/handler/src/response.test.ts
@@ -1,13 +1,41 @@
 import { HandlerResponse, HandlerResponsePresetNothing } from './component/response';
-import { create, nothing } from './response';
+import { create, nothing, unwrap } from './response';
 
 type TestResponseOne = HandlerResponse<'response:test:one', boolean>;
-type TestResponseTwo = HandlerResponse<'response:test:two', {
+
+type TestResponseTwo = HandlerResponse<'response:test:two', TestResponseTwoBody>;
+type TestResponseTwoBody = {
   readonly foo: string;
   readonly bar: string;
-}>;
+};
 
 describe('response', (): void => {
+  describe('unwrap()', (): void => {
+    it('with response one, unwrap, boolean', (): void => {
+      expect(
+        unwrap<TestResponseOne>({
+          type: 'response:test:one',
+          value: false,
+        }),
+      ).toStrictEqual(false);
+    });
+
+    it('with response two, unwrap, object', (): void => {
+      expect(
+        unwrap<TestResponseTwo>({
+          type: 'response:test:two',
+          value: {
+            foo: 'foobar',
+            bar: 'barfoo',
+          },
+        }),
+      ).toStrictEqual<TestResponseTwoBody>({
+        foo: 'foobar',
+        bar: 'barfoo',
+      })
+    });
+  });
+
   describe('create()', () => {
     it('with response one, type and name, returns response', (): void => {
       expect(
--- packages/@phasma/handler/src/response.ts
+++ packages/@phasma/handler/src/response.ts
@@ -1,6 +1,11 @@
 import { HandlerResponseConstraint, HandlerResponsePresetNothing } from './component/response';
 
 /**
+ * Unwrap the response and return its containedd value.
+ */
+export const unwrap = <T extends HandlerResponseConstraint>(response: T): T['value'] => response.value;
+
+/**
  * Create a response of given type {@link T}.
  */
 export const create = <T extends HandlerResponseConstraint>(type: T['type'], value: T['value']): T => {
