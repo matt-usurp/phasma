5c16ede27c120835b40e497fa1b718b812abee64
Matthew Rose <matt.usurp@gmail.com>
2022-04-13 13:28:07 +0100

    docs(handler-aws): lambda handler response updates

--- packages/@phasma/handler-aws/src/response.ts
+++ packages/@phasma/handler-aws/src/response.ts
@@ -1,13 +1,9 @@
 import { create } from '@phasma/handler/src/response';
-import { LambdaHandlerResponse } from './component/response';
+import type { LambdaHandlerResponse } from './component/response';
 
-export {
-  nothing,
-  unwrap,
-} from '@phasma/handler/src/response';
-
+export { nothing, unwrap } from '@phasma/handler/src/response';
 
 /**
- * Return an AWS formated handler result.
+ * Return an AWS formatted handler result of type {@link T}.
  */
 export const result = <T>(value: T): LambdaHandlerResponse<T> => create('response:aws:result', value);
