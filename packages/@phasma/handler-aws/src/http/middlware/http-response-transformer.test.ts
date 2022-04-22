5f332ced4ec377621256374b6cc3bbab6ad02a6d
Matthew Rose <matt.usurp@gmail.com>
2022-04-22 16:36:43 +0100

    chore(handler-aws): update existing http middleware with new practices

--- packages/@phasma/handler-aws/src/http/middlware/http-response-transformer.test.ts
+++ packages/@phasma/handler-aws/src/http/middlware/http-response-transformer.test.ts
@@ -3,67 +3,63 @@ import { http, HttpResponse } from '@phasma/handler/src/http/response';
 import { HttpResponseLambdaProxy, HttpResponseTransformerMiddleware } from './http-response-transformer';
 
 describe(HttpResponseTransformerMiddleware.name, (): void => {
-  it('with http response, returns lambda response response', async (): Promise<void> => {
-    const middleware = new HttpResponseTransformerMiddleware();
+  describe('create()', (): void => {
+    it('with http response, returns lambda response response', async (): Promise<void> => {
+      const middleware = HttpResponseTransformerMiddleware.create();
 
-    const next = jest.fn();
+      const next = jest.fn();
 
-    next.mockImplementationOnce(async (): Promise<HttpResponse> => {
-      return http({
-        status: 204,
-        body: undefined,
+      next.mockImplementationOnce(async (): Promise<HttpResponse> => {
+        return http({
+          status: 204,
+          body: undefined,
+        });
       });
+
+      expect(
+        await middleware.invoke({
+          provider: 'given-provider',
+          context: 'given-context' as any, // eslint-disable-line @typescript-eslint/no-explicit-any
+          next,
+        }),
+      ).toStrictEqual<HttpResponseLambdaProxy>({
+        type: 'response:aws:result',
+        value: {
+          statusCode: 204,
+          headers: {},
+          body: undefined,
+        },
+      });
+
+      expect(next).toBeCalledTimes(1);
+      expect(next).toBeCalledWith('given-context');
     });
 
-    expect(
-      await middleware.invoke({
-        // Provider is ignore for this middleware
-        provider: 'given-provider',
+    it('with unknown response, returns unknown response', async (): Promise<void> => {
+      const middleware = HttpResponseTransformerMiddleware.create();
 
-        // Context is ignored for this middleware
-        context: 'given-context' as any, // eslint-disable-line @typescript-eslint/no-explicit-any
-        next,
-      }),
-    ).toStrictEqual<HttpResponseLambdaProxy>({
-      type: 'response:aws:result',
-      value: {
-        statusCode: 204,
-        headers: {},
-        body: undefined,
-      },
-    });
+      const next = jest.fn();
 
-    expect(next).toBeCalledTimes(1);
-    expect(next).toBeCalledWith('given-context');
-  });
+      next.mockImplementationOnce(async (): Promise<HandlerResponse<'response:unknown', 1000>> => {
+        return {
+          type: 'response:unknown',
+          value: 1000,
+        };
+      });
 
-  it('with unknown response, returns unknown response', async (): Promise<void> => {
-    const middleware = new HttpResponseTransformerMiddleware();
-
-    const next = jest.fn();
-
-    next.mockImplementationOnce(async (): Promise<HandlerResponse<'response:unknown', 1000>> => {
-      return {
+      expect(
+        await middleware.invoke({
+          provider: 'given-provider',
+          context: 'given-context' as any, // eslint-disable-line @typescript-eslint/no-explicit-any
+          next,
+        }),
+      ).toStrictEqual<HandlerResponse<'response:unknown', 1000>>({
         type: 'response:unknown',
         value: 1000,
-      };
+      });
+
+      expect(next).toBeCalledTimes(1);
+      expect(next).toBeCalledWith('given-context');
     });
-
-    expect(
-      await middleware.invoke({
-        // Provider is ignore for this middleware
-        provider: 'given-provider',
-
-        // Context is ignored for this middleware
-        context: 'given-context' as any, // eslint-disable-line @typescript-eslint/no-explicit-any
-        next,
-      }),
-    ).toStrictEqual<HandlerResponse<'response:unknown', 1000>>({
-      type: 'response:unknown',
-      value: 1000,
-    });
-
-    expect(next).toBeCalledTimes(1);
-    expect(next).toBeCalledWith('given-context');
   });
 });
