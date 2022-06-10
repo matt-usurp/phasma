6ef7f927be69e0f500b8ec76add1774758a9d76d
Matthew Rose <matt.usurp@gmail.com>
2022-06-10 13:57:58 +0100

    chore: move to vitest

---
+++ vitest.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig as configure } from 'vitest/config';
+
+export default configure({
+  test: {
+    globals: true,
+    environment: 'node',
+    passWithNoTests: true,
+
+    include: [
+      'packages/**/*.test.{js,jsx,ts,tsx}',
+    ],
+
+    coverage: {
+      all: true,
+      clean: true,
+
+      include: [
+        'packages/**/*',
+      ],
+
+      exclude: [
+        'packages/**/examples',
+        'packages/**/*.proof.*',
+        'packages/index.ts',
+      ],
+
+      reportsDirectory: 'build/coverage',
+      reporter: ['text', 'html-spa'],
+
+      lines: 100,
+      functions: 100,
+      branches: 100,
+      statements: 100,
+    },
+  },
+});
