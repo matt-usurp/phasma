cb6caccc0b095d4be5f970ddeb18c0ab13e127f1
Matthew Rose <matt.usurp@gmail.com>
2022-09-29 09:00:41 +0100

    feat(handler-aws): merge http body transformer and encoder middleware

---
+++ packages/@phasma/handler-aws/src/http/middlware/http-request-body-transformer.test.ts
@@ -0,0 +1,419 @@
+import { fn, partial } from '@matt-usurp/grok/testing';
+import type { HandlerResponseConstraint } from '@phasma/handler/src/component/response';
+import type { HttpBodyDecoder } from '@phasma/handler/src/http/body';
+import { error } from '@phasma/handler/src/http/response';
+import type { HttpValidatorFunction, HttpValidatorFunctionResultFailure, HttpValidatorFunctionResultSuccess } from '@phasma/handler/src/http/validator';
+import type { FromType, ZodIssue } from '@phasma/handler/src/http/validator/zod';
+import { z } from 'zod';
+import type { Event, Provider } from '../../index';
+import { HttpRequestBodyTransformerMiddleware, HttpRequestBodyTransformerMiddlewareUsingZod, HttpRequestBodyValidatorContext, HttpRequestBodyValidatorResponseError } from './http-request-body-transformer';
+
+describe(HttpRequestBodyTransformerMiddleware.name, (): void => {
+  describe('constructor()', (): void => {
+    it('with payload, undefined, return http error, body missing', async (): Promise<void> => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const decoder = fn<HttpBodyDecoder<any>>();
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const validator = fn<HttpValidatorFunction<any, unknown>>();
+
+      const middleware = new HttpRequestBodyTransformerMiddleware(decoder, validator);
+
+      const next = vi.fn();
+
+      expect(
+        await middleware.invoke({
+          provider: partial<Provider.ForEvent<'apigw:proxy:v2'>>({
+            id: 'provider:aws:lambda',
+
+            event: partial<Event.Payload<'apigw:proxy:v2'>>({
+              body: undefined,
+            }),
+          }),
+
+          context: 'test:middleware:context' as any, // eslint-disable-line @typescript-eslint/no-explicit-any
+          next,
+        }),
+      ).toStrictEqual<HttpRequestBodyValidatorResponseError.BodyMissing>(
+        error<HttpRequestBodyValidatorResponseError.BodyMissing>(
+          'body',
+          'missing',
+        ),
+      );
+
+      expect(decoder).toBeCalledTimes(0);
+
+      expect(validator).toBeCalledTimes(0);
+
+      expect(next).toBeCalledTimes(0);
+    });
+
+    it('with payload, empty string, return http error, missing', async (): Promise<void> => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const decoder = fn<HttpBodyDecoder<any>>();
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const validator = fn<HttpValidatorFunction<any, unknown>>();
+
+      const middleware = new HttpRequestBodyTransformerMiddleware(decoder, validator);
+
+      const next = vi.fn();
+
+      expect(
+        await middleware.invoke({
+          provider: partial<Provider.ForEvent<'apigw:proxy:v2'>>({
+            id: 'provider:aws:lambda',
+
+            event: partial<Event.Payload<'apigw:proxy:v2'>>({
+              body: '',
+            }),
+          }),
+
+          context: 'test:middleware:context' as any, // eslint-disable-line @typescript-eslint/no-explicit-any
+          next,
+        }),
+      ).toStrictEqual<HttpRequestBodyValidatorResponseError.BodyMissing>(
+        error<HttpRequestBodyValidatorResponseError.BodyMissing>(
+          'body',
+          'missing',
+        ),
+      );
+
+      expect(decoder).toBeCalledTimes(0);
+
+      expect(validator).toBeCalledTimes(0);
+
+      expect(next).toBeCalledTimes(0);
+    });
+
+    it('with payload, decoder called, returns error, return http error, malformed', async (): Promise<void> => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const decoder = fn<HttpBodyDecoder<any>>();
+
+      decoder.mockImplementationOnce((): undefined => {
+        return undefined;
+      });
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const validator = fn<HttpValidatorFunction<any, unknown>>();
+
+      const middleware = new HttpRequestBodyTransformerMiddleware(decoder, validator);
+
+      const next = vi.fn();
+
+      expect(
+        await middleware.invoke({
+          provider: partial<Provider.ForEvent<'apigw:proxy:v2'>>({
+            id: 'provider:aws:lambda',
+
+            event: partial<Event.Payload<'apigw:proxy:v2'>>({
+              body: 'test:provider:event:body:malformed',
+            }),
+          }),
+
+          context: 'test:middleware:context' as any, // eslint-disable-line @typescript-eslint/no-explicit-any
+          next,
+        }),
+      ).toStrictEqual<HttpRequestBodyValidatorResponseError.BodyMalformed>(
+        error<HttpRequestBodyValidatorResponseError.BodyMalformed>(
+          'body',
+          'malformed',
+        ),
+      );
+
+      expect(decoder).toBeCalledTimes(1);
+      expect(decoder).toBeCalledWith<[string]>('test:provider:event:body:malformed');
+
+      expect(validator).toBeCalledTimes(0);
+
+      expect(next).toBeCalledTimes(0);
+    });
+
+    it('with payload, decoder called, validator called, returns error, return http error, validation error', async (): Promise<void> => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const decoder = fn<HttpBodyDecoder<any>>();
+
+      decoder.mockImplementationOnce((): string => {
+        return 'test:decoder:success';
+      });
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const validator = fn<HttpValidatorFunction<any, unknown>>();
+
+      validator.mockImplementationOnce((): HttpValidatorFunctionResultFailure<string[]> => {
+        return {
+          success: false,
+          errors: [
+            'test:validator:error:a',
+            'test:validator:error:b',
+            'test:validator:error:c',
+            'test:validator:error:d',
+          ],
+        };
+      });
+
+      const middleware = new HttpRequestBodyTransformerMiddleware(decoder, validator);
+
+      const next = vi.fn();
+
+      expect(
+        await middleware.invoke({
+          provider: partial<Provider.ForEvent<'apigw:proxy:v2'>>({
+            id: 'provider:aws:lambda',
+
+            event: partial<Event.Payload<'apigw:proxy:v2'>>({
+              body: 'test:provider:event:body:invalid',
+            }),
+          }),
+
+          context: 'test:middleware:context' as any, // eslint-disable-line @typescript-eslint/no-explicit-any
+          next,
+        }),
+      ).toStrictEqual<HttpRequestBodyValidatorResponseError.BodyValidationFailure<string[]>>(
+        error<HttpRequestBodyValidatorResponseError.BodyValidationFailure<string[]>>(
+          'body',
+          'validation',
+          [
+            'test:validator:error:a',
+            'test:validator:error:b',
+            'test:validator:error:c',
+            'test:validator:error:d',
+          ],
+        ),
+      );
+
+      expect(decoder).toBeCalledTimes(1);
+      expect(decoder).toBeCalledWith<[string]>('test:provider:event:body:invalid');
+
+      expect(validator).toBeCalledTimes(1);
+      expect(validator).toBeCalledWith<[string]>('test:decoder:success');
+
+      expect(next).toBeCalledTimes(0);
+    });
+
+    it('with payload, decoder called, validator called, next called, with body context', async (): Promise<void> => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const decoder = fn<HttpBodyDecoder<any>>();
+
+      decoder.mockImplementationOnce((): string => {
+        return 'test:decoder:success';
+      });
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const validator = fn<HttpValidatorFunction<any, unknown>>();
+
+      validator.mockImplementationOnce((): HttpValidatorFunctionResultSuccess<string[]> => {
+        return {
+          success: true,
+          data: [
+            'test:validator:data:a',
+            'test:validator:data:b',
+            'test:validator:data:c',
+            'test:validator:data:d',
+          ],
+        };
+      });
+
+      const middleware = new HttpRequestBodyTransformerMiddleware(decoder, validator);
+
+      const next = vi.fn();
+
+      next.mockImplementationOnce(async (): Promise<HandlerResponseConstraint> => {
+        return {
+          type: 'response:example',
+          value: 'test:middleware:next:response',
+        };
+      });
+
+      expect(
+        await middleware.invoke({
+          provider: partial<Provider.ForEvent<'apigw:proxy:v2'>>({
+            id: 'provider:aws:lambda',
+
+            event: partial<Event.Payload<'apigw:proxy:v2'>>({
+              body: 'test:provider:event:body',
+            }),
+          }),
+
+          context: {
+            inherit: 'test:context:inherit',
+          } as any, // eslint-disable-line @typescript-eslint/no-explicit-any
+
+          next,
+        }),
+      ).toStrictEqual<HandlerResponseConstraint>({
+        type: 'response:example',
+        value: 'test:middleware:next:response',
+      });
+
+      expect(decoder).toBeCalledTimes(1);
+      expect(decoder).toBeCalledWith<[string]>('test:provider:event:body');
+
+      expect(validator).toBeCalledTimes(1);
+      expect(validator).toBeCalledWith<[string]>('test:decoder:success');
+
+      expect(next).toBeCalledTimes(1);
+      expect(next).toBeCalledWith<[HttpRequestBodyValidatorContext<string[]> & { inherit: string }]>({
+        inherit: 'test:context:inherit',
+
+        body: [
+          'test:validator:data:a',
+          'test:validator:data:b',
+          'test:validator:data:c',
+          'test:validator:data:d',
+        ],
+      });
+    });
+  });
+});
+
+describe(HttpRequestBodyTransformerMiddlewareUsingZod.name, (): void => {
+  describe('zod()', (): void => {
+    it('with body, json malformed, returns http error, malformed', async (): Promise<void> => {
+      type TestBodyMapping = {
+        readonly name: string;
+        readonly age: number;
+      };
+
+      const middleware = new HttpRequestBodyTransformerMiddlewareUsingZod<TestBodyMapping>(
+        z.object<FromType<TestBodyMapping>>({
+          name: z.string(),
+          age: z.number(),
+        }),
+      );
+
+      const next = vi.fn();
+
+      expect(
+        await middleware.invoke({
+          provider: partial<Provider.ForEvent<'apigw:proxy:v2'>>({
+            id: 'provider:aws:lambda',
+
+            event: partial<Event.Payload<'apigw:proxy:v2'>>({
+              body: '{b"r[o]ke"n}}',
+            }),
+          }),
+
+          context: 'test:middleware:context' as any, // eslint-disable-line @typescript-eslint/no-explicit-any
+          next,
+        }),
+      ).toStrictEqual<HttpRequestBodyValidatorResponseError.BodyMalformed>(
+        error<HttpRequestBodyValidatorResponseError.BodyMalformed>(
+          'body',
+          'malformed',
+        ),
+      );
+
+      expect(next).toBeCalledTimes(0);
+    });
+
+    it('with body, json decoded, schema invalid, returns http error, validation error', async (): Promise<void> => {
+      type TestBodyMapping = {
+        readonly name: string;
+        readonly age: number;
+      };
+
+      const middleware = new HttpRequestBodyTransformerMiddlewareUsingZod<TestBodyMapping>(
+        z.object<FromType<TestBodyMapping>>({
+          name: z.string(),
+          age: z.number(),
+        }),
+      );
+
+      const next = vi.fn();
+
+      expect(
+        await middleware.invoke({
+          provider: partial<Provider.ForEvent<'apigw:proxy:v2'>>({
+            id: 'provider:aws:lambda',
+
+            event: partial<Event.Payload<'apigw:proxy:v2'>>({
+              body: JSON.stringify({}),
+            }),
+          }),
+
+          context: 'test:middleware:context' as any, // eslint-disable-line @typescript-eslint/no-explicit-any
+          next,
+        }),
+      ).toStrictEqual<HttpRequestBodyValidatorResponseError.BodyValidationFailure<ZodIssue[]>>(
+        error<HttpRequestBodyValidatorResponseError.BodyValidationFailure<ZodIssue[]>>(
+          'body',
+          'validation',
+          [
+            expect.objectContaining({
+              code: 'invalid_type',
+              message: 'Required',
+              received: 'undefined',
+              path: ['name'],
+            }),
+            expect.objectContaining({
+              code: 'invalid_type',
+              message: 'Required',
+              received: 'undefined',
+              path: ['age'],
+            }),
+          ],
+        ),
+      );
+
+      expect(next).toBeCalledTimes(0);
+    });
+
+    it('with body, json decoded, schema validates, next called, with body context', async (): Promise<void> => {
+      type TestBodyMapping = {
+        readonly name: string;
+        readonly age: number;
+      };
+
+      const middleware = new HttpRequestBodyTransformerMiddlewareUsingZod<TestBodyMapping>(
+        z.object<FromType<TestBodyMapping>>({
+          name: z.string(),
+          age: z.number(),
+        }),
+      );
+
+      const next = vi.fn();
+
+      next.mockImplementationOnce(async (): Promise<HandlerResponseConstraint> => {
+        return {
+          type: 'response:example',
+          value: 'test:middleware:next:response',
+        };
+      });
+
+      expect(
+        await middleware.invoke({
+          provider: partial<Provider.ForEvent<'apigw:proxy:v2'>>({
+            id: 'provider:aws:lambda',
+
+            event: partial<Event.Payload<'apigw:proxy:v2'>>({
+              body: JSON.stringify({
+                name: 'test:body:name',
+                age: 40,
+              }),
+            }),
+          }),
+
+          context: {
+            inherit: 'test:context:inherit',
+          } as any, // eslint-disable-line @typescript-eslint/no-explicit-any
+
+          next,
+        }),
+      ).toStrictEqual<HandlerResponseConstraint>({
+        type: 'response:example',
+        value: 'test:middleware:next:response',
+      });
+
+      expect(next).toBeCalledTimes(1);
+      expect(next).toBeCalledWith<[HttpRequestBodyValidatorContext<TestBodyMapping> & { inherit: string }]>({
+        inherit: 'test:context:inherit',
+
+        body: {
+          name: 'test:body:name',
+          age: 40,
+        },
+      });
+    });
+  });
+});
---
+++ packages/@phasma/handler-aws/src/http/middlware/http-request-body-transformer.ts
@@ -0,0 +1,90 @@
+import type { Grok } from '@matt-usurp/grok';
+import type { HttpBodyDecoder } from '@phasma/handler/src/http/body';
+import { decode as json } from '@phasma/handler/src/http/body/json';
+import { error } from '@phasma/handler/src/http/response';
+import type { HttpValidatorFunction } from '@phasma/handler/src/http/validator';
+import { validate, ZodIssue, ZodSchema } from '@phasma/handler/src/http/validator/zod';
+import type { Http, Middleware, Provider } from '../../index';
+
+export type HttpRequestBodyValidatorResponseError<Error> = (
+  | HttpRequestBodyValidatorResponseError.BodyMissing
+  | HttpRequestBodyValidatorResponseError.BodyMalformed
+  | HttpRequestBodyValidatorResponseError.BodyValidationFailure<Error>
+);
+
+export namespace HttpRequestBodyValidatorResponseError {
+  export type BodyMissing = Http.Response.Error<'body', 'missing', undefined>;
+  export type BodyMalformed = Http.Response.Error<'body', 'malformed', undefined>;
+  export type BodyValidationFailure<Error> = Http.Response.Error<'body', 'validation', Error>;
+}
+
+export type HttpRequestBodyValidatorContext<Body> = {
+  /**
+   * The request body parsed, validated and made available via {@link HttpRequestBodyTransformerMiddleware}.
+   */
+  readonly body: Body;
+};
+
+export type HttpRequestBodyValidatorMiddlewareDefinition<Body, ValidationError> = (
+/* eslint-disable @typescript-eslint/indent */
+  Middleware.Definition<
+    Provider.ForEvent<'apigw:proxy:v2'>,
+    Middleware.Definition.Any.ContextInbound,
+    HttpRequestBodyValidatorContext<Body>,
+    Middleware.Definition.Any.ResponseInbound,
+    HttpRequestBodyValidatorResponseError<ValidationError>
+  >
+/* eslint-enable @typescript-eslint/indent */
+);
+
+export class HttpRequestBodyTransformerMiddleware<Body, ValidationError> implements Middleware.Implementation<HttpRequestBodyValidatorMiddlewareDefinition<Body, ValidationError>> {
+  public constructor(
+    public readonly decoder: HttpBodyDecoder<Grok.Constraint.ObjectLike>,
+    public readonly validator: HttpValidatorFunction<Grok.Constraint.ObjectLike, ValidationError>,
+  ) {}
+
+  /**
+   * @inheritdoc
+   */
+  public async invoke({ provider, context, next }: Middleware.Fn.Input<HttpRequestBodyValidatorMiddlewareDefinition<Body, ValidationError>>): Middleware.Fn.Output<HttpRequestBodyValidatorMiddlewareDefinition<Body, ValidationError>> {
+    const body = provider.event?.body;
+
+    if (body === undefined || body === '') {
+      return error<HttpRequestBodyValidatorResponseError.BodyMissing>(
+        'body',
+        'missing',
+      );
+    }
+
+    const payload = this.decoder(body);
+
+    if (payload === undefined) {
+      return error<HttpRequestBodyValidatorResponseError.BodyMalformed>(
+        'body',
+        'malformed',
+      );
+    }
+
+    const result = this.validator(payload);
+
+    if (result.success === false) {
+      return error<HttpRequestBodyValidatorResponseError.BodyValidationFailure<ValidationError>>(
+        'body',
+        'validation',
+        result.errors,
+      );
+    }
+
+    return next({
+      ...context,
+
+      body: result.data as unknown as Body,
+    });
+  }
+}
+
+export class HttpRequestBodyTransformerMiddlewareUsingZod<Body> extends HttpRequestBodyTransformerMiddleware<Body, ZodIssue[]> {
+  public constructor(schema: ZodSchema) {
+    super(json, validate(schema));
+  }
+}
